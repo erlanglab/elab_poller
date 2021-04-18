6a56c399ac75acf9989476aa677723aae6c8f8a7
Piotr Pasternak <piotrpassternak@gmail.com>
2021-04-18 20:13:51 +0200

    Add allocator sizes measurement

--- src/elab_poller.erl
+++ src/elab_poller.erl
@@ -1,6 +1,6 @@
 -module(elab_poller).
 
--export([microstate_accounting/0]).
+-export([microstate_accounting/0, allocator_sizes/0]).
 
 
 -spec microstate_accounting() -> ok.
@@ -11,3 +11,9 @@ microstate_accounting() ->
     List ->
       [telemetry:execute([vm, microstate_accounting], Map, #{time => calendar:local_time()}) || Map <- List]
   end.
+
+-spec allocator_sizes() -> ok.
+allocator_sizes() ->
+  Keys = erlang:system_info(alloc_util_allocators),
+  Sizes_list = [{Alloc, erlang:system_info({allocator_sizes, Alloc})} || Alloc <- Keys],
+  telemetry:execute([vm, allocator_sizes], maps:from_list(Sizes_list), #{time => calendar:local_time(), keys => Keys}).
