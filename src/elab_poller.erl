92d72a0229a5ec537209339e0be44511316be528
Piotr Pasternak <piotrpassternak@gmail.com>
2021-04-18 18:53:12 +0200

    Add microstate_accounting measurement

--- src/elab_poller.erl
+++ src/elab_poller.erl
@@ -1,3 +1,13 @@
 -module(elab_poller).
 
--export([]).
+-export([microstate_accounting/0]).
+
+
+-spec microstate_accounting() -> ok.
+microstate_accounting() ->
+  case erlang:statistics(microstate_accounting) of
+    undefined -> ok;
+
+    List ->
+      [telemetry:execute([vm, microstate_accounting], Map, #{}) || Map <- List]
+  end.
