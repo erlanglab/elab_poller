015e8715ffdb14a6766d9058914597f12f6fcbda
Piotr Pasternak <piotrpassternak@gmail.com>
2021-04-27 20:23:13 +0200

    Aggregating measurements per scheduler type

--- src/elab_poller.erl
+++ src/elab_poller.erl
@@ -9,7 +9,9 @@ microstate_accounting() ->
     undefined -> ok;
 
     List ->
-      [telemetry:execute([vm, microstate_accounting], maps:put(system_time, erlang:system_time(), Map), #{}) || Map <- List]
+      Types = lists:usort([Type || #{type := Type} <- List]),
+      AggregatedCountList = aggregate_counts(List, Types, []),
+      [telemetry:execute([vm, microstate_accounting], maps:put(system_time, erlang:system_time(), Map), #{}) || Map <- AggregatedCountList]
   end.
 
 -spec allocator_sizes() -> ok.
@@ -17,3 +19,19 @@ allocator_sizes() ->
   Keys = erlang:system_info(alloc_util_allocators),
   SizesList = [{Alloc, erlang:system_info({allocator_sizes, Alloc})} || Alloc <- Keys],
   telemetry:execute([vm, allocator_sizes], maps:put(system_time, erlang:system_time(), maps:from_list(SizesList)), #{}).
+
+
+
+-spec aggregate_counts([map()], [atom()], [map()]) -> [map()].
+aggregate_counts(_, [], AggregationList) -> AggregationList;
+aggregate_counts(List, [H|T], AggregationList) ->
+  {Satisfying, NotSatisfying} = lists:partition(fun(X) -> maps:get(type, X) == H end, List),
+  AggregatedSum = lists:foldr(fun aggregate_sum/2, #{}, Satisfying),
+  aggregate_counts(NotSatisfying, T, AggregationList ++ [#{counters => AggregatedSum, type => H}]).
+
+-spec aggregate_sum(map(), map()) -> map().
+aggregate_sum(Map, AggregatedCounters) when map_size(AggregatedCounters) == 0 ->
+  maps:get(counters, Map);
+aggregate_sum(Map, AggregatedCounters) ->
+  Counters = maps:get(counters, Map),
+  maps:map(fun(Key, Value) -> Value + maps:get(Key, Counters) end, AggregatedCounters).
