fda4debe84b5c2e3ba90b770089ef24170f3e5f6
Piotr Pasternak <piotrpassternak@gmail.com>
2021-05-24 15:34:04 +0200

    Write to .csv for microstate accounting

--- src/aggregators.erl
+++ src/aggregators.erl
@@ -1,7 +1,7 @@
 -module(aggregators).
 
 %% API
--export([microstate_accounting_aggregator/4, alocator_sizes_aggregator/4]).
+-export([microstate_accounting_aggregator/4, allocator_sizes_aggregator/4]).
 
 microstate_accounting_aggregator(
     [vm, microstate_accounting],
@@ -13,12 +13,20 @@ microstate_accounting_aggregator(
     _metadata,
     _config
 ) ->
-    io:format(
-        "counters: ~w, type: ~s, system_time: ~w~n~n",
-        [maps:to_list(Counters), Type, Time]
-    ).
+  % .csv format -> "scheduler type","scheduler state","counter","system time"
+    case file:open("microstate_accounting_results.csv", [append]) of
+      {ok, Fd} ->
+        [io:format(
+          Fd,
+          "~s,~s,~w,~w~n",
+          [Type, State, Counter, Time]
+        ) || {State, Counter} <- maps:to_list(Counters)];
 
-alocator_sizes_aggregator([vm, allocator_sizes], Map, _metadata, _config) ->
+      {error, Reason} ->
+        io:format("Couldn't open file due to ~w~n", [Reason])
+    end.
+
+allocator_sizes_aggregator([vm, allocator_sizes], Map, _metadata, _config) ->
     [io:format("key: ~w, value: ~w~n~n", [Key, maps:get(Key, Map)]) || Key <- maps:keys(Map)].
 
 
